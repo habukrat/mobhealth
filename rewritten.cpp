4fddbbcf2e10bcbfacd5349bdded041d43dbde53
Hadar Abukrat <hadarabukrat@Chaim-As-MacBook.local>
2018-09-12 21:48:55 -0700

    simplified

---
+++ rewritten.cpp
@@ -0,0 +1,83 @@
+#ifndef __f0700_h__
+#define __f0700_h__
+
+// *********************************************************************
+//
+//  Thanks for your interest in this position. In this preliminary 
+//  screen process, you are given the following native 32-bit C code:
+//
+//    1. Refactor/rewrite the code into a SOLID readable code
+//       - A READABLE code should allow other programmers to fully 
+//         understand what the code is doing, by just glancing over it 
+//         (within 3-5 seconds) without having to read through lengthy
+//         comments / documentation, thus improving the overall
+//         productivity of the team
+//       - A READABLE code should be "self-explanatory", short, 
+//         consistent, and properly structured that allow seamless 
+//         communication across team
+//       - Keep in mind, there is no absolutely right/wrong way. But
+//         we mostly interested in candidates who can boost the overall
+//         productivity of the code review team. Here are few
+//         suggestions you may consider:
+//         - eliminate unnecessary pointer arithmetics
+//         - eliminate unnecessary variables/loop/calculation
+//         - keep it super simple, DON'T write code that only hardcore 
+//           programmers can understand; DO write high level code that 
+//           every programmers one can read
+//
+//    2. What strategy would you use to verify that your code is correct?
+//
+//    3. Explain what the code is trying to accomplish intuitively
+//
+//    4. Please show us how you accomplish all #1, #2, #3:
+//       - Setup a project with Visual Studio 2015 (or higher) on Win7
+//         so that we can verify that your code is working properly
+//       - Version control your steps with Git to help our reviewers 
+//         to validate/review your code
+//       - Zip your Git repo and email it back to us for further review
+//
+//  Good Luck!
+//
+// ********************************************************************
+
+
+signed int __cdecl f0700(int a1, int *a2, int a3, int *a4)
+{
+   // Pre-Condition:
+   //    a1: possible input value = [64, 128, 256]
+   //    a2: is an int[1408] array
+   //        possible input value for each element = [-4095 ... 4095]
+   //    a3: possible input value = [2, 3, 4, 5, 6]
+   //    a4: is an int[116] array
+   //        possible input value for each element = [0]
+   //
+   // Post-Condition:
+   //    a1: value shouldn't change
+   //    a2: value shouldn't change
+   //    a3: value shouldn't changed
+   //    a4: value can changed
+
+
+   int *arr;
+   int *array;
+   int neg1 = -1;
+
+
+   for(int x = 0; x < a1; x++) {
+      array[0] = a2[x];
+      if (neg1 * (array[x] - array[x+1]) < a3) {
+         for(int y = neg1 * (array[0] - array[x+2]); y < a3; y++) {
+            if(neg1 * array[x+1] > neg1 * array[x]) {
+               array[x] = array[x+1];
+               x++;
+            }
+         }
+      }
+      neg1 = - neg1;
+   }
+
+   arr[a1-1] = 10000;
+   arr[a1] = -10000;
+
+   return -1;
+}
